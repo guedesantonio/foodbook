df21b27a76cd82c3a11f0865276c1ba190e3222c
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-17 15:53:49 +1100

    React route added to client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Router>
+        <Switch>
+          <Route exact path="/login" component={Home} />
+          <Route exact path="/signup" component={Home} />
+          <Route exact path="/members" component={Saved} />
+        </Switch>
+    </Router>
   );
 }
 
 
 export default App;
+/* <Route component={NoMatch} /> */
