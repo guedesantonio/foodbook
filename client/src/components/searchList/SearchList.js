8db5e84f1cea512efd6b4e98263f5ac0bfd5109e
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-21 18:40:55 +1100

    Api call working properly

--- client/src/components/searchList/SearchList.js
+++ client/src/components/searchList/SearchList.js
@@ -1,35 +1,94 @@
-import React from 'react';
-import { Card, ListGroup, ListGroupItem, Form, FormGroup, Label, Input } from 'reactstrap';
-import RecipeService from "../../services/RecipeService"
+import React, { useRef } from 'react';
+import { Card, ListGroup, ListGroupItem, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import RecipeService from "../../services/RecipeService";
 
-console.log(RecipeService.getRecipeList());
+class SearchList extends React.Component {
 
-const SearchList = (props) => {
-  return (
-    <Card style={{ backgroundColor: "#3C5A98", color: "RED" }}>
-      <div className="row">
-      <div className="col s12 ">
-      <Form style={{ padding: "10px", color: "white" }}>
-        <FormGroup>
-          <Label for="exampleSearch">Search</Label>
-          <Input
-            type="search"
-            name="search"
-            id="exampleSearch"
-            placeholder="search placeholder"/>
-      </FormGroup>
-      </Form>
-      <ListGroup>
-        <ListGroupItem>Cras justo odio</ListGroupItem>
-        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-        <ListGroupItem>Morbi leo risus</ListGroupItem>
-        <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-        <ListGroupItem>Vestibulum at eros</ListGroupItem>
-      </ListGroup>
-      </div>
-    </div>
-    </Card>
-  );
+  state = {
+    inputRecipeName: ''
+  }
+
+  inputRecipeNameChange = (e)=> {
+    this.setState({ inputRecipeName: e.target.value });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const recipeList = await RecipeService.getRecipeList(this.state.inputRecipeName);
+    console.log(recipeList);
+  }
+
+  // const recipeSearchInput = useRef();
+  // const searchRecipes = async () => {
+    // console.log(recipeSearchInput.current);
+    // const recipeList = await RecipeService.getRecipeList(recipeSearchInput.current.value);
+    // console.log(recipeList);
+  
+
+  render(){
+    return(
+      <Card style={{ backgroundColor: "#3C5A98", color: "white" }}>
+        <div className="row">
+          <div className="col s12 ">
+            <Form
+              style={{ padding: "20px" }}
+              onSubmit={ this.handleSubmit }
+            >
+              <FormGroup >
+                <Label for="exampleSearch">Search Recipe</Label>
+                <Input
+                  onChange={ this.inputRecipeNameChange }
+                  style={{ color: "white" }}
+                  type="search"
+                  name="search"
+                  placeholder="Type Recipe Name Here" />
+              </FormGroup>
+              <Button
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                  backgroundColor: "#3C5A98",
+                  borderColor: "white",
+                  color: "white"
+                }}>Search</Button>
+            </Form>
+            <ListGroup >
+              <ListGroupItem style={{ backgroundColor: "#3C5A98", color: "white" }}>
+                <div className="card mb-3" style={{ maxWidth: "540px" }}>
+                  <div className="row no-gutters">
+                    <div className="col-md-4">
+                      <img src="https://spoonacular.com/recipeImages/654812-312x231.jpg" className="card-img" alt="..." />
+                    </div>
+                    <div className="col-md-8">
+                      <div className="card-body">
+                        <h5 className="card-title">Pasta with Calamaris</h5>
+                        <Button style={{
+                          width: "140px",
+                          borderRadius: "3px",
+                          letterSpacing: "1.5px",
+                          backgroundColor: "#3C5A98",
+                          color: "white"
+                        }}>See recipe</Button>
+
+                        <Button style={{
+                          width: "140px",
+                          borderRadius: "3px",
+                          letterSpacing: "1.5px",
+                          backgroundColor: "#3C5A98",
+                          color: "white"
+                        }}>Save Recipe</Button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </ListGroupItem>
+            </ListGroup>
+          </div>
+        </div>
+      </Card>
+    );
+  };
 }
 
 export default SearchList;
--- client/src/services/RecipeService.js
+++ client/src/services/RecipeService.js
@@ -4,9 +4,9 @@ const apiKey = "5251ecf8756e4993a1833647f2083118"
 
 // class containing main services for the app
 export default class RecipeService {
-    static async getRecipeList() {
+    static async getRecipeList(recipeName) {
         try {
-            const res = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=${apiKey}`);
+            const res = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${recipeName}&maxFat=25&number=8&apiKey=${apiKey}`);
             return res.data;
         } catch (error) {
             console.error(error);
