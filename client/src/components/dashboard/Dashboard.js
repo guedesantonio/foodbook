938d2db8955cb7e2e93d6349ab1f15ac79228610
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-22 15:36:57 +1100

    Search list incorporated to tab component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { Component  } from "react";
 import ProfileHeader from "../profileHeader/ProfileHeader";
 import ProfileTab from "../profileTab/ProfileTab";
-import SearchList from "../searchList/SearchList";
 
 class Dashboard extends Component {
   
@@ -10,7 +9,6 @@ return (
       <div style={{ height: "75vh" }} className="container ">
         <ProfileHeader/>
         <ProfileTab/>
-        <SearchList/>
       </div>
     );
   }
--- client/src/components/profileTab/ProfileTab.js
+++ client/src/components/profileTab/ProfileTab.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col} from 'reactstrap';
 import classnames from 'classnames';
 import RecipesCarousel from "../carousel/Carousel.js"
+import SearchList from '../searchList/SearchList.js';
 
 const ProfileTab = (props) => {
   const [activeTab, setActiveTab] = useState('1');
@@ -36,6 +37,7 @@ const ProfileTab = (props) => {
         <TabPane tabId="1">
           <Row>
             <Col sm="12">
+              <SearchList/>
             </Col>
           </Row>
         </TabPane>
