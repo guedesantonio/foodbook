4507099420dba57664be1ed57175ac234f1d8506
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-27 21:50:38 +1100

    Heroku deploy typo

--- server.js
+++ server.js
@@ -37,5 +37,7 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
-// process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.get('*', res => res.sendFile(path.join(__dirname + '/client/build/index.html')));
+app.listen(PORT, () =>
+  console.log(`:earth_americas:  ==> API Server now listening on PORT ${PORT}!`)
+);
