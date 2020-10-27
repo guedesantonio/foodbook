443657dbe4e84369e5e3f0f99517fc3f98284673
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-27 23:02:31 +1100

    path added to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 
