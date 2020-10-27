3d940201d10b2e978db03015c94c1e57cd70b295
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-27 22:57:56 +1100

    Heroku 8

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,7 +12,7 @@ const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
 // Load User model
-const User = require('../../models/User');
+const User = require('../../models/users');
 
 // @route POST api/users/register
 // @desc Register user
