6b093e781a72d18f0402d84019b465fd295ffd8a
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-27 22:14:21 +1100

    Heroku deploy trial 4

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ require('./config/passport')(passport);
 // Routes
 app.use('/api/users', users);
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/client/build/index.html')));
 app.listen(PORT, () =>
   console.log(`:earth_americas:  ==> API Server now listening on PORT ${PORT}!`)
